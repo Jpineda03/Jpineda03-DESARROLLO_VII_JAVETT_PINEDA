98cbaae56b4e42f8b3b5f35954cbd6a2e48ca9b2
unknown <javett.pineda3@gmail.com>
2024-08-24 13:59:24 -0500

    Ejercicio de calificador completado

--- TALLERES/TALLER_2/calificador.php
+++ TALLERES/TALLER_2/calificador.php
@@ -1,6 +1,7 @@
 <?php
-$calificacion = 100
+$calificacion = 100;
 
+// Evaluación de calificación usando if-else
 if ($calificacion >= 90) {
     echo "Tu calificación es A.<br>";
 } elseif ($calificacion >= 80) {
@@ -14,11 +15,30 @@ if ($calificacion >= 90) {
 }
 echo "<br>";
 
+// Evaluación del resultado aprobado/reprobado
 if ($calificacion >= 60) {
     $resultado = "Aprobado";
 } else {
     $resultado = "Reprobado";
 }
-echo "calificación: $calificacion <br>";
-echo "Resultado (if-else): $resultado<br>";
+echo "Calificación: $calificacion <br>";
+echo "Resultado: $resultado<br>";
+
+// Evaluación de la calificación usando switch
+switch (true) {
+    case ($calificacion >= 90):
+        echo "Excelente trabajo.<br>";
+        break;
+    case ($calificacion >= 80):
+        echo "Buen trabajo.<br>";
+        break;
+    case ($calificacion >= 70):
+        echo "Trabajo aceptable.<br>";
+        break;
+    case ($calificacion >= 60):
+        echo "Necesitas mejorar.<br>";
+        break;
+    default:
+        echo "Debes esforzarte más.<br>";
+}
 ?>
